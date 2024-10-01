78507181899c821cca3fab829507ba578cbc043a
Simon <152021717+SimonR-prog@users.noreply.github.com>
2024-10-01 12:41:22 +0200

    Add files via upload

---
+++ WPF/ToDoApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ToDoApp;
+
+public partial class MainWindow : Window
+{
+    private ObservableCollection<string> _activities = [];
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        LV_Activities.ItemsSource = _activities;
+        Input_Activity.Focus();
+    }
+
+    private void Btn_Save_Click(object sender, RoutedEventArgs e)
+    {
+        HandleSave();
+    }
+    private void HandleSave()
+    {
+        var activity = Input_Activity.Text;
+
+        if (!string.IsNullOrWhiteSpace(activity))
+        {
+            _activities.Add(activity);
+        }
+        Input_Activity.Text = "";
+        Input_Activity.Focus();
+    }
+
+    private void Input_Activity_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        if (e.Key == System.Windows.Input.Key.Enter)
+        {
+            HandleSave();
+        }
+    }
+}
